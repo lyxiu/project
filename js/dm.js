0be84952ea330f78754306eed463fed5cc787d08
lyxiu <52280989+lyxiu@users.noreply.github.com>
2019-09-03 22:06:56 +0800

    Add files via upload

---
+++ js/dm.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+$(document).ready(function(){
+    /*main页面*/
+    var strips = document.querySelector('.strips');
+    var strip  = document.querySelectorAll('.strip');
+
+    strip.forEach(function(theStrip){
+        theStrip.addEventListener('mouseenter', function() {
+            strip[0].classList.remove('hoverin');
+            theStrip.classList.add('hoverin');
+        });
+        theStrip.addEventListener('mouseleave', function() {
+            theStrip.classList.remove('hoverin');
+            strip[0].classList.add('hoverin');
+        });
+    });
+
+
+});
